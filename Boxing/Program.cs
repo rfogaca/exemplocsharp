a45e7dde9a8d13ad5197900f6f47a851f7edc9ff
rfogaca <rodrigofogacars@gmail.com>
2015-10-05 20:03:53 -0300

    Trabalhando com boxing e unboxing.

---
+++ Boxing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            object o = (object)a;
+            //System.Object
+
+            Console.WriteLine(o.GetType().ToString());
+            
+            int b = (int)o;
+
+            System.Type t = b.GetType();
+
+            //((int)o)
+
+            Console.WriteLine(b);
+
+            Console.ReadKey();
+        }
+    }
+}
