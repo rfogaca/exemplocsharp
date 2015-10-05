c4bc81c12f60891d5fd31a94142295897b0d83a7
rfogaca <rodrigofogacars@gmail.com>
2015-10-05 19:14:37 -0300

    Trabalhando com tipo de variáveis e conversões.

--- ValueTypes/Program.cs
+++ ValueTypes/Program.cs
@@ -8,15 +8,36 @@ namespace ValueTypes
 {
     class Program
     {
+
+        enum acessorio { Sapato, Bolsa, Cinto, Carteira, Colar };
+
         static void Main(string[] args)
         {
             int i = 10;
             string a = "OK";
+            char ch = 'X';
 
             float ff= 10.89F;
             double dd = 90.9876;
 
             decimal dec = 10.00M;
+
+            bool bl = true;
+
+            int item = (int)acessorio.Cinto;
+            
+            //implicit conversion
+            int i1 = 10;
+            long i2 = i1;
+
+            //explicit conversion
+            double d1 = 10.0;
+            int d2 = (int)d1;
+
+            //int.TryParse(
+            //int.Parse(
+
+            //Convert.
 
             Console.WriteLine(a);
             Console.ReadKey();
