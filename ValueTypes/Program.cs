6ad204211f95030e3c59b00e5e6da2a3b2ba3f49
rfogaca <rodrigofogacars@gmail.com>
2015-10-05 18:12:50 -0300

    Trabalhando com tipos de variáveis.

---
+++ ValueTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            string a = "OK";
+
+            float ff= 10.89F;
+            double dd = 90.9876;
+
+            decimal dec = 10.00M;
+
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
