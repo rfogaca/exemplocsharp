29bfa59e63c5ef70f4cec50e88728bbbcf66ebcc
rfogaca <rodrigofogacars@gmail.com>
2015-12-14 17:27:03 -0200

    ItetationStatement while e do while

---
+++ IterationStatements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+
+            do
+            {
+                //a = a + 2;
+                //a++;
+                //Console.WriteLine(a.ToString());
+            } while (a > 1);
+
+            int b = 10;
+
+            while(b >= 1)
+            {
+                Console.WriteLine(b.ToString());
+                b--; //b--;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
