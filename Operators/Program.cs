352c1b6223bf5f82567c79b091e7c68117add13e
rfogaca <rodrigofogacars@gmail.com>
2015-12-15 19:36:37 -0200

    Aula de exemplo utilizando operadores.

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -10,6 +10,8 @@ namespace Operators
     {
         static void Main(string[] args)
         {
+
+            #region Sintaxe
             int a = 10;
             int b = 2;
 
@@ -38,40 +40,77 @@ namespace Operators
             //Comparativos
 
             if (a > b)
-                Console.WriteLine("a > b");
+            {
+                //Console.WriteLine("a > b");
+            }
 
             if (b < a)
-                Console.WriteLine("b < a");
+            {
+                //Console.WriteLine("b < a");
+            }
 
             // => - Lambda
-            if (a >= b) 
-                Console.WriteLine("b < a");
+            if (a >= b)
+            {
+                //Console.WriteLine("b < a");
+            }
 
             if (b <= a)
-                Console.WriteLine("b < a");
+            {
+                //Console.WriteLine("b < a");
+            }
 
             //Equals
             if (a == b)
-                Console.WriteLine("b < a");
+            {
+                //Console.WriteLine("b < a");
+            }
 
             //Not
             if (a != b)
-                Console.WriteLine("b < a");
+            {
+                //Console.WriteLine("b < a");
+            }
 
             //Logicos
             //XOR - logico exclusivo - Um ou outro
-            if(true ^ false)
-                Console.WriteLine("True");
+            if (true ^ false)
+            {
+                //Console.WriteLine("True");
+            }
 
             //Condicionais
             //AND
             if (true && true)
-                Console.WriteLine("True");
+            {
+                //Console.WriteLine("True");
+            }
 
             //OR --> if(a < b || b > c)
-            if(false || true)
-                Console.WriteLine("True");
+            if (false || true)
+            {
+                //Console.WriteLine("True");
+            }
+            #endregion
 
+            Console.WriteLine("Valor 1 ");
+            int v1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor 2 ");
+            int v2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Soma: " + Convert.ToString(v1 + v2));
+            Console.WriteLine("Subtração: " + Convert.ToString(v1 - v2));
+            Console.WriteLine("Multiplicação: " + Convert.ToString(v1 * v2));
+            Console.WriteLine("Divisão: " + Convert.ToString(v1 / v2));
+
+            if (v1 > 0 && v2 > 0)
+                Console.WriteLine("Os valores v1 e v2 Maiores que Zero");
+
+            if (v1 + v2 <= 0 || v1 - v2 <= 0 || v1 * v2 <= 0 || v1 / v2 <= 0)
+                Console.WriteLine("Uma ou mais operações possuem valor Menor ou Igual a Zero");
+
+            Console.ReadKey();
         }
     }
 }
